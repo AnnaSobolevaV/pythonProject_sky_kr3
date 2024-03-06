10f113f57b30f4e819fc2f5a19497551c30f54c6
Anna Soboleva <asoboleva.avs@gmail.com>
2024-03-07 01:42:23 +0300

    main() was updated

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from utils.functions import load_data
+from utils.functions import build_total_list
+from utils.functions import print_list
+
+DATA_PATH = "./data/operations.json"
+
+
 def main():
-    pass
+    data = load_data(DATA_PATH)
+    print(data)
+    list_transactions = build_total_list(data)
+    print_list(list_transactions)
 
 
 if __name__ == '__main__':
